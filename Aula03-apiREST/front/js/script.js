13efcbb4c6116e600036fd205a7da75c4e8d8042
Pedro Guterres Jaeger Livi <pedro.livi2005@gmail.com>
2026-04-16 11:21:45 -0300

    cinco

--- Aula03-apiREST/front/js/script.js
+++ Aula03-apiREST/front/js/script.js
@@ -2,92 +2,85 @@ function carregarProdutos(){
     var req = new XMLHttpRequest()
 
     req.onreadystatechange = function(){
-        if( this.readyState == 4 && this.status == 200){ //quando o servidor recebe a requisição o redstate é 4 
+        if( this.readyState == 4 && this.status == 200){
             var produtos = JSON.parse( this.responseText )
-            var txt = '<div class="row"> '
-            txt +=    '     <div class="col"> <strong> Código </strong></div>'
-            txt +=    '     <div class="col"> <strong> Nome </strong> </div>'
-            txt +=    '     <div class="col"> <strong> Preco </strong> </div>'
-            txt +=    '     <div class="col"> <strong> Categoria </strong> </div>'
-            txt +=    '     <div class="col"> <strong> Editar </strong> </div>'
-            txt +=    '</div> '
-            
+            var txt =   '<div class="row">'
+            txt +=      '   <div class="col"> <strong> Código </strong></div>'
+            txt +=      '   <div class="col"> <strong> Nome </strong></div>'
+            txt +=      '   <div class="col"> <strong> Preço </strong></div>'
+            txt +=      '   <div class="col"> <strong> Categoria </strong></div>'
+            txt +=      '</div> '
+
             produtos.forEach( prod => {
-                txt +=     '<div class="row"> '
-                txt +=    '     <div class="col"> ' + prod.id + ' </div>'
-                txt +=    '     <div class="col"> ' + prod.nome + ' </div>'
-                txt +=    '     <div class="col"> ' + prod.preco + ' </div>'
-                txt +=    '     <div class="col"> ' + prod.categoria + ' </div>'
-                txt +=    '     <div class="col">  <botton class="btn btn-success" onclick="editarProdutos()> Editar </botton> </div>'
-                txt +=    '</div> '
+                txt +=      '<div class="row">'
+                txt +=      '   <div class="col"> ' + prod.id + ' </div>'
+                txt +=      '   <div class="col"> ' + prod.nome + ' </div>'
+                txt +=      '   <div class="col"> ' + prod.preco + ' </div>'
+                txt +=      '   <div class="col"> ' + prod.categoria + '</div>'
+                txt +=      '</div> '
             })
             document.getElementById("divConteudo").innerHTML = txt
+                  
         }
     }
-
-    
-
     req.open("GET", "http://localhost:8001/product", true)
     req.send()
 }
-
-function editarProdutos(){
-    var req = new XMLHttpRequest()
-    req.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            var dadosParaEnviar = JSON.parse( this.responseText )
-            var txt = '<div class="row"> '
-            txt +=    '     <div class="col"> <strong> Código </strong></div>'
-            txt +=    '     <div class="col"> <strong> Nome </strong> </div>'
-            txt +=    '     <div class="col"> <strong> Preco </strong> </div>'
-            txt +=    '     <div class="col"> <strong> Categoria </strong> </div>'
-            txt +=    '</div> '
-            
-            dadosParaEnviar.forEach( prod => {
-                txt +=     '<div class="row"> '
-                txt +=    '     <div class="col"> ' + prod.id + ' </div>'
-                txt +=    '     <div class="col"> <input type="text" class="form-control" name="' + prod.nome + '" value="' + prod.nome + '"> </div>'
-                txt +=    '     <div class="col"> ' + prod.preco + ' </div>'
-                txt +=    '     <div class="col"> ' + prod.categoria + ' </div>'
-                txt +=    '     <div class="col">  <botton class="btn btn-success" onclick="salvarProdutos()> Salvar </botton> </div>'
-                txt +=    '</div> '
-            })
-            document.getElementById("divConteudo").innerHTML = txt
-        
-        }
-    }
-
-    req.open("PATCH", "http://localhost:8001/product", true)
-    req.send(dadosParaEnviar)
-}
-
-function salvarProdutos(){
-
-}
-
 
 function carregarCategorias(){
     var req = new XMLHttpRequest()
-    req.onreadystatechange = function(){ //manipulador de eventos no JavaScript utilizado com o objeto XMLHttpRequest (AJAX) para 
-                                        //monitorar alterações na propriedade readyState
-        if( this.readyState == 4 && this.status == 200){ //quando o servidor recebe a requisição o redstate é 4 
+
+    req.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
             var categorias = JSON.parse( this.responseText )
-            var txt = '<div class="row"> '
-            txt +=    '     <div class="col"> <strong> Código </strong></div>'
-            txt +=    '     <div class="col"> <strong> Nome </strong> </div>'
-            txt +=    '</div> '
-            
+            var txt =   '<div class="row">'
+            txt +=      '   <div class="col"> <strong> Código </strong></div>'
+            txt +=      '   <div class="col"> <strong> Nome </strong></div>'
+            txt +=      '   <div class="col"> <strong> Editar </strong></div>'
+            txt +=      '</div> '
+
             categorias.forEach( cat => {
-                txt +=     '<div class="row"> '
-                txt +=    '     <div class="col"> ' + cat.id + ' </div>'
-                txt +=    '     <div class="col"> ' + cat.nome + ' </div>'
-                txt +=    '</div> '
+                txt +=      '<div class="row">'
+                txt +=      '   <div class="col"> ' + cat.id + ' </div>'
+                txt +=      '   <div class="col" id="divNome' + cat.id +'"> ' + cat.nome + ' </div>'
+                txt +=      '   <div class="col" id="divBtn'+cat.id+'">'
+                txt +=              '<button class="btn btn-success" onclick="formEditar(' + cat.id +')">Editar</button>' 
+                txt +=      '   </div>'
+                txt +=      '</div> '
             })
             document.getElementById("divConteudo").innerHTML = txt
+                  
         }
     }
-
-    req.open("GET", "http://localhost:8001/product", true)
+    req.open("GET", "http://localhost:8001/category", true)
     req.send()
 }
 
+function formEditar( idCat ){
+    var divNome = document.getElementById( "divNome" + idCat)
+    var nome = divNome.innerHTML
+    var novaDivNome = '<input type="text" id="txtNome" value="' + nome +'" > '
+    divNome.innerHTML = novaDivNome
+    var divEditar = document.getElementById("divBtn" + idCat)
+    divEditar.innerHTML = '<button class="btn btn-danger" onclick="editar(' + idCat + ')">Salvar</button>'
+}
+
+function editar( idCat ){
+    var novoNome = document.getElementById("txtNome").value
+    if( novoNome != "" ){
+        var req = new XMLHttpRequest()
+        req.onreadystatechange = function(){
+            if( this.readyState == 4 ){
+                carregarCategorias()
+            }
+        }
+        req.open("PUT", "http://localhost:8001/category/" + idCat , true)
+
+        //req.setRequestHeader( "Content-type" , "application/x-www-form-urlencoded" );
+        //req.send( "nome=" + novoNome );
+
+        req.setRequestHeader( "Content-type" , "application/json" )
+        req.send( JSON.stringify({ nome : novoNome }) )
+
+    }
+}
